6538d6052269e7eca380bb36610c3de4caee5f03
Strange <168190107+fxgurv@users.noreply.github.com>
2024-11-26 13:13:20 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
-PEXELS_API_KEY = "YOUR_PEXELS_API_KEY"
-FONT_SIZE = "50"
+INPUT = "/content/intro.mp3"  # Your input video/audio file
+WHISPER_MODEL = "base"
+DIMENSION = "1920x1080"
+MUSIC_DIR = "Music"
+FONT = "Helvetica-Bold"
+FONT_SIZE = 50
 TEXT_COLOR = "#ffffff"
 STROKE_COLOR = "#ff3d00"
-STROKE_WIDTH = "#ffffff"
-#highlight "word" "word-background", "off/false"
-HIGHLIGHT = "WORD"
+STROKE_WIDTH = 2
+SUBTITLE_STYLE = "word"  # Options: "word", "word-background", "off"
 HIGHLIGHT_COLOR = "#ff3d00"
-MAX_CHARACTERS_PER_LINE = "15"
-MAXIMUM_LINES_PER_PAGE = "2"
+MAX_CHARACTERS_PER_LINE = 15
+MAXIMUM_LINES_PER_PAGE = 2
+X_POSITION = 25
+Y_POSITION = 125
+PEXELS_API_KEY = "YOUR_API_KEY"  # Replace with your Pexels API key
